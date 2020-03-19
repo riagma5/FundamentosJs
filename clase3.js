cb439c9781d21395888e9184fcee2b69be5b2dd0
Ricardo Aguilera <riagma5@gmail.com>
2020-03-19 16:41:33 -0600

    Clase 3, varibales números

---
+++ clase3.js
@@ -0,0 +1,20 @@
+var edad = 27
+edad += 1
+
+var peso = 75
+peso -= 2
+
+var sandwich = 1
+peso += sandwich
+
+var jugarFutbol = 3
+peso -= jugarFutbol
+
+var precioDeVino = 200.3
+var total = Math.round(precioDeVino * 100 * 3) /100
+var totalStr = total.toFixed(2)
+var total2 = parseFloat(totalStr)
+
+var pizza = 8
+var persona = 2
+var cantidadPorcionesPersona = pizza / persona
