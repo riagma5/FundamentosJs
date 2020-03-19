488d86aacdcb3faeea1631f697f59f18531308f4
Ricardo Aguilera <riagma5@gmail.com>
2020-03-19 17:13:13 -0600

    Clase sobre funciones

---
+++ alcanceFunciones.js
@@ -0,0 +1,8 @@
+var nombre = 'Ricardo'
+
+function imprimirNombre(name){
+  name = name.toUpperCase()
+  console.log(name)
+}
+
+imprimirNombre(nombre)
---
+++ funciones.js
@@ -0,0 +1,10 @@
+var nombre = 'Ricardo', edad = 28
+
+function imprimirEdad(name, year){
+  console.log(`${name} tiene ${year} años`)
+}
+
+imprimirEdad(nombre, edad)
+imprimirEdad('Vicky', 28)
+imprimirEdad('Eric', 24)
+imprimirEdad('Dario', 27)
