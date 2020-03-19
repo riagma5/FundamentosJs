21b0c367cf10f8bba53f65970cc730a179a44e16
Ricardo Aguilera <riagma5@gmail.com>
2020-03-19 16:05:28 -0600

    Comenzando clase de Js, clase1 variables

---
+++ clase1.js
@@ -0,0 +1,10 @@
+// console.log('Hola mundo JS!')
+
+var nombre = 'Ricardo',
+    apellido = 'Aguilera'
+var edad = '28 años'
+
+console.log('Hola ' + nombre + ' ' + apellido)
+console.log('Tengo ' + edad)
+
+var peso = 75
