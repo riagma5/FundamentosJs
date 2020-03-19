989bc33decdfa56b438365991cd5eb342f5600ac
Ricardo Aguilera <riagma5@gmail.com>
2020-03-19 16:19:54 -0600

    Clase 2, variables Strings

--- clase1.js
+++ clase1.js
@@ -1,10 +1,26 @@
 // console.log('Hola mundo JS!')
 
-var nombre = 'Ricardo',
+/*var nombre = 'Ricardo',
     apellido = 'Aguilera'
 var edad = '28 años'
 
 console.log('Hola ' + nombre + ' ' + apellido)
 console.log('Tengo ' + edad)
 
-var peso = 75
+var peso = 75*/
+
+var nombre = 'Ricardo', apellido = 'Aguilera'
+
+var nombreEnMayusculas = nombre.toUpperCase()
+var apellidoEnMinusculas = apellido.toLowerCase()
+
+var primeraLetraDelNombre = nombre.charAt(0)
+
+var cantidadDeLetrasDelNombre = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(1, 2)
+
+//Reto: mostrar al usuario la ultima letra de su nombreEnMayusculas
+var ultimaLetra = nombre.charAt(nombre.length-1)
